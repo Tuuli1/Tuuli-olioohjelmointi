1dc7dce3c167f472bc4cba641adc592df65b4486
Tuuli Hermalahti <tuuli.hermalahti@student.careeria.fi>
2022-01-11 13:15:31 +0200

    Alustava commit

---
+++ Harjoitus1/Harjoitus1/Ajoneuvot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harjoitus1
+{
+    internal class Ajoneuvot
+    {
+
+        public string Nimi { get; set; }
+        public int Nopeus { get; set; }
+        public int Renkaat { get; set; }
+
+        public void TulostaData()
+        {
+            Console.WriteLine("Ajoneuvo: ");
+            Console.WriteLine("-- Nimi: " + Nimi);
+            Console.WriteLine("-- Nopeus: " + Nopeus);
+            Console.WriteLine("-- Renkaat: " + Nimi);
+
+        }
+
+        //public string ToString()
+        //{
+
+        //}
+
+
+    }
+
+}
---
+++ Harjoitus1/Harjoitus1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Harjoitus1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Olio ajoneuvo luokasta
+            Ajoneuvot auto = new Ajoneuvot();
+
+            //Asetetaan oliolle auto arvot
+            auto.Nimi = "Toyota";
+            auto.Nopeus = 160;
+            auto.Renkaat = 4;
+
+            //Käytetään auto olion toimintoja
+            auto.TulostaData();
+        }
+    }
+}
