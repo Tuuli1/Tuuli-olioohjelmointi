1978785ec230fddc348596fdc3f81da6804fc57b
Tuuli Hermalahti <tuuli.hermalahti@student.careeria.fi>
2022-01-11 16:16:33 +0200

    Opiskelija tehtävä

--- Harjoitus2/Harjoitus2/Program.cs
+++ Harjoitus2/Harjoitus2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Harjoitus2
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Opiskelija opiskelija1 = new Opiskelija("Matti", "TVT1234", 0);
+
+            opiskelija1.TulostaData();
+            opiskelija1.MuokkaaOpintopisteitä(5);
+            opiskelija1.TulostaData();
+        }
+    }
+}
+
